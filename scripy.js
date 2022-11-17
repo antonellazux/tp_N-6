a4371485457a01a9b21801693f6760f1100859ff
antonellazux <zuniga.antonella.epet20@gmail.com>
2022-11-17 16:00:31 -0300

    pregunta 10

---
+++ scripy.js
@@ -0,0 +1,14 @@
+//#region 
+var hoy = document.getElementById(hoy);
+var suma;
+//#endregion
+
+//#region 
+if ((numero1 && nuemero2)=12) {
+    suma=numero1+nuemero2;
+}
+//#endregion
+
+//#region 
+console.log(suma);
+//#endregion
